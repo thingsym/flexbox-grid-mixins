71b544b9618f43a20db9069632e524495b65d079
thingsym <thingsym@gmail.com>
2018-12-24 23:44:35 +0900

    chore: change lint from scss-lint to stylelint

--- Gemfile
+++
@@ -1,6 +0,0 @@
-# how to install : bundle install --path vendor/bundle
-
-source "https://rubygems.org"
-
-gem "scss_lint"
-gem "scss_lint_reporter_checkstyle"
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-var scsslint = require('gulp-scss-lint');
 require('es6-promise').polyfill();
 
 var runSequence = require('run-sequence');
@@ -24,13 +23,29 @@ var dest_paths = {
 
 gulp.task('lint:sass', function() {
   return gulp.src(src_paths.sass)
-    .pipe($.plumber())
-    .pipe(scsslint({
-        'reporterOutputFormat': 'Checkstyle',
-        'bundleExec': true,
-        'config': 'scss-lint.yml'
+    .pipe($.plumber({
+      errorHandler: function(err) {
+        console.log(err.messageFormatted);
+        this.emit('end');
+      }
     }))
-    .pipe(scsslint.failReporter());
+    .pipe($.stylelint({
+      config: {
+        extends: [
+          "stylelint-config-recommended",
+          "stylelint-scss",
+          "stylelint-config-recommended-scss"
+        ],
+        rules: {
+          "block-no-empty": null,
+          "no-descending-specificity": null
+        }
+      },
+      reporters: [{
+        formatter: 'string',
+        console: true
+      }]
+    }));
 });
 
 gulp.task('docs_sass', function() {
